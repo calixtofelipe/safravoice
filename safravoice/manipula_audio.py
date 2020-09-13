77a40500f9dcf1d6039f0e64e22240156655272d
Felipe Calixto <calixtofelipe@hotmail.com>
2020-09-13 07:52:07 -0300

    fix: remove sounddevice para rodar no heroku

--- safravoice/manipula_audio.py
+++ safravoice/manipula_audio.py
@@ -1,5 +1,5 @@
 import base64
-import sounddevice as sd
+# import sounddevice as sd
 import wavio
 
 
@@ -26,22 +26,3 @@ def decodeAudio(encoded_audio, nome_arquivo):
     message_bytes = base64.b64decode(base64_bytes)
     wav_file = open(nome_arquivo, "wb+")
     wav_file.write(message_bytes)
-
-
-def gravaAudio(audio_filename, segundos):
-    """
-    Função usada para gravar audios de testes unitários ao longo do processo
-
-    :param audio_filename: Nome do arquivo de audio
-    :param segundos: Tempo  em segundos do audio
-    :author: Ellen Giacometti
-    """
-    fs = 44100  # Taxa de amostragem
-    for i in range(len(audio_filename)):
-        # A captura de audio deve começar após 500ms,esse tempo de silêncio é para minimizar os ruídos
-        print("[AUDIO_INPUT] Gravando audio")
-        audio = sd.rec(int(segundos * fs), samplerate=fs, channels=2)
-        sd.wait()  # Aguarda enquanto o audio está sendo gravado
-        print("[AUDIO_INPUT] Salvando audio no formato .wav")
-        wavio.write(audio_filename[i], audio, fs,
-                    sampwidth=2)  # Salva o audio na taxa de amostragem gravada
